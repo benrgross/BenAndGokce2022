c3084ca231e908cfdac530542f5742a789d09c2c
Ben Gross <benrgross@gmail.com>
2021-12-13 20:07:03 -0500

    added navbar and react-strap

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
 import React from "react";
-// import ReactDOM from "react-dom";
-import logo from "./logo.svg";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>hello world.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div>Save the date goes here</div>;
+}
+
+export default Home;
