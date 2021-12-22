7acd1478c99230645481b4af2d92979d4eeab256
Ben Gross <benrgross@gmail.com>
2021-12-22 11:16:42 -0500

    added about text component w/ lorem

--- client/src/components/AboutText.js
+++ client/src/components/AboutText.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function About() {
+function AboutText() {
   return (
     <div>
       <p>
@@ -28,4 +28,4 @@ function About() {
   );
 }
 
-export default About;
+export default AboutText;
