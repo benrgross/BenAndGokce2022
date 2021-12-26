c15ab200f6ac41814323fd38ab854ae4b1be70fb
Ben Gross <benrgross@gmail.com>
2021-12-26 15:59:02 -0500

    style admin login

--- client/src/pages/AdminLogin.js
+++ client/src/pages/AdminLogin.js
@@ -1,27 +1,43 @@
 import React, { useRef } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Row, Col, Container } from "react-bootstrap";
 
 function AdminLogin() {
   const userRef = useRef();
   const passRef = useRef();
   return (
     <div>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>user name</Form.Label>
-          <Form.Control ref={userRef} type="username" placeholder="user name" />
-          <Form.Text className="text-muted"></Form.Text>
-        </Form.Group>
+      <Container fluid style={{ marginTop: "10%" }}>
+        <Row>
+          <Col></Col>
+          <Col>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>user name</Form.Label>
+                <Form.Control
+                  ref={userRef}
+                  type="username"
+                  placeholder="user name"
+                />
+                <Form.Text className="text-muted"></Form.Text>
+              </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>password</Form.Label>
-          <Form.Control ref={passRef} type="password" placeholder="password" />
-        </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>password</Form.Label>
+                <Form.Control
+                  ref={passRef}
+                  type="password"
+                  placeholder="password"
+                />
+              </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
