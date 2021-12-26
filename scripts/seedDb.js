6ce2fcc784517e3ccdef56972c8aa77a7b8ec445
Ben Gross <benrgross@gmail.com>
2021-12-26 16:31:51 -0500

    admin seed inserted

--- scripts/seedDb.js
+++ scripts/seedDb.js
@@ -3,6 +3,11 @@ const db = require("../models");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wedding");
 
+const adminSeed = {
+  username: "admin",
+  password: "process.env.ADMIN_PASSWORD",
+};
+
 const guestSeed = [
   {
     first_name: "Ben",
@@ -294,6 +299,17 @@ const guestSeed = [
     plus_1: true,
   },
 ];
+
+db.Admin.remove({})
+  .then(() => db.Admin.collection.insertMany(adminSeed))
+  .then((data) => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
 
 db.Guest.remove({})
   .then(() => db.Guest.collection.insertMany(guestSeed))
