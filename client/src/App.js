5dee6aeee8b3e31c8930d052224585f06373b5c6
Ben Gross <benrgross@gmail.com>
2021-12-26 15:55:18 -0500

    added an admin route

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import RSVP from "./pages/RSVP";
+import AdminLogin from "./pages/AdminLogin";
 import "./App.css";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/rsvp" component={RSVP} />
+          <Route exact path="/admin" component={AdminLogin} />
         </Switch>
       </>
     </Router>
---
+++ client/src/pages/AdminLogin.js
@@ -0,0 +1,29 @@
+import React, { useRef } from "react";
+import { Form, Button } from "react-bootstrap";
+
+function AdminLogin() {
+  const userRef = useRef();
+  const passRef = useRef();
+  return (
+    <div>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>user name</Form.Label>
+          <Form.Control ref={userRef} type="username" placeholder="user name" />
+          <Form.Text className="text-muted"></Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>password</Form.Label>
+          <Form.Control ref={passRef} type="password" placeholder="password" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default AdminLogin;
