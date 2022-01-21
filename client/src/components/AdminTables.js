6b56d75063992efeabb8a1c99d28798448a5c291
Ben Gross <benrgross@gmail.com>
2022-01-21 21:18:07 +0300

    adjusting grid for tables

--- client/src/components/AdminTables.js
+++ client/src/components/AdminTables.js
@@ -7,28 +7,30 @@ import Table from "react-bootstrap/Table";
 import TBody from "./TBody";
 
 function AdminTables({ guests }) {
-  useEffect(() => {
-    countGuests();
-  }, []);
+  // useEffect(() => {
+  //   countGuests();
+  // }, []);
 
-  const countGuests = () => {
-    let sum = 0;
-    guests.forEach((guest) => {
-      guest.plus_1 = 0 ? (sum += 1) : (sum += 1 + guest.plus_1);
-    });
+  // const countGuests = () => {
+  //   let sum = 0;
+  //   guests.forEach((guest) => {
+  //     guest.plus_1 = 0 ? (sum += 1) : (sum += 1 + guest.plus_1);
+  //   });
 
-    console.log(sum);
-  };
+  //   console.log(sum);
+  // };
   return (
-    <Container fluid id="main-table">
-      <Row>
-        <Col>
-          <Table striped bordered hover responsive>
-            <TBody guests={guests} />
-          </Table>
-        </Col>
-      </Row>
-    </Container>
+    // <Container fluid id="main-table">
+    <Row className="d-flex justify-content center">
+      {/* <Col xs={0} md={4}></Col> */}
+      <Col xs={12} md={8}>
+        <Table striped bordered hover>
+          <TBody guests={guests} />
+        </Table>
+      </Col>
+      {/* <Col xs={0} md={4}></Col> */}
+    </Row>
+    // </Container>
   );
 }
 
